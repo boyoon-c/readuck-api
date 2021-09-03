76ef6fc056a9d1d88585a49c4e7fe9b268f78e70
boyoon-c <boyoonchang@gmail.com>
2021-09-03 11:24:10 -0700

    Setup landing page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+# Article
+class Article(models.Model):
+  author = models.CharField(max_length=250)
+  title = models.CharField(max_length=150)
+  abstract = models.TextField(max_length=500)
+  citation = models.IntegerField()
+  journalname=models.CharField(max_length=250)
+  ## user should be many-to-many
+  user = models.ForeignKey(User, on_delete=models.CASCADE) 
+  ## group should be many-to-many
+
+  def __str__(self):
+    return self.name
+
+'''
+# Group
+class Group(models.Model):
+  ## article many-to-many
+  article = models.ForeignKey(Article, on_delete=models.CASCADE)
+  ## user many-to-many
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+# Comment
+class Comment(models.Model):
+  review=models.ForeignKey(Review,on_Delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+# Review
+class Review(models.Model):
+  article = models.ForeignKey(Article, on_Delete = models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  comment = models.ForeignKey(Comment, on_Delete=models.CASCADE)
+  
+# Article file
+class File(models.Model):
+  url=models.CharField(max_length=250)
+'''
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
+'''
+def article_index(request):
+  articles = Article.objects.all()
+  return render(request, '')
+'''
+
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def signup(request):
+  return render(request, 'signup.html')
