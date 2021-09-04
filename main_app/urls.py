f31a8f0691d2780c3aaaa12ea4f9078a0a9f2da3
boyoon-c <boyoonchang@gmail.com>
2021-09-04 12:34:58 -0700

    Refactor sign up page; add pages on nav bar

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
+  path('login/', views.Login.as_view(), name='login'),
   path('about/', views.about, name='about'),
   path('signup/', views.signup, name='signup'),
-  path('login/', views.login, name='login')
+  # path('login/', views.login, name='login')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Article
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from main_app import models
+import uuid
+import boto3
 
 # Create your views here.
 '''
@@ -7,7 +17,9 @@ def article_index(request):
   articles = Article.objects.all()
   return render(request, '')
 '''
-
+class Login(LoginView):
+    template_name='login.html'
+    
 def home(request):
   return render(request, 'home.html')
 
@@ -15,7 +27,18 @@ def about(request):
   return render(request, 'about.html')
 
 def signup(request):
-  return render(request, 'signup.html')
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('home')
+    else:
+      error_message = 'Invalid sign up -try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
 
 def login(request):
   return render(request, 'login.html')
