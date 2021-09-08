199ef7a7ed5d527e91e3b61938e6f68a67a13fb8
boyoon-c <boyoonchang@gmail.com>
2021-09-07 23:14:35 -0700

    Add joined group info and style profile page

--- main_app/views.py
+++ main_app/views.py
@@ -155,7 +155,7 @@ class GroupDetail(DetailView):
   def get_context_data(self, **kwargs):
     # Call the base implementation first to get a context
     context = super().get_context_data(**kwargs)
-    # Add in a QuerySet of all the books
+    # Add in a QuerySet of all the groups
     context['group_article_list'] = GroupArticle.objects.filter(group_id=self.object.id)
     print('context', context['group_article_list'])
     return context
@@ -216,13 +216,16 @@ def profile(request):
   articles = Article.objects.filter(user=request.user)
   reviews = Review.objects.filter(user=request.user)
   replies = Reply.objects.filter(user=request.user)
-  for reply in replies:
-    print(reply.review.article)
-  # temp = replies.select_related('review__id')
-  
-  
+  groups=Group.objects.all()
+  user_group=[]
+  for g in groups:
+    for p in g.participants.all():
+      if p==request.user:
+        user_group.append(g)
+
   return render(request, 'profile.html', {
     'articles': articles,
     'reviews': reviews,
-    'replies': replies
+    'replies': replies,
+    'user_group': user_group
   })
