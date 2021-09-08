c8f7998a281c5a13c40dc36e40ed04502aa5f391
boyoon-c <boyoonchang@gmail.com>
2021-09-07 17:21:01 -0700

    Make it private

--- readuckapi/settings.py
+++ readuckapi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!cl*=++2hw%a@4l0gayv7ytufv3!i5vg647j5rf==c)%qx$g4&'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
